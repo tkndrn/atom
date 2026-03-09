bcd74c0c18a2620025c1b7a42a33e2105785357d
tkndrn <tekinduran77@gmail.com>
2026-03-09 18:10:53 +0300

    Update link.py

--- link.py
+++ link.py
@@ -1,9 +1,9 @@
 import requests
 
-# base url çek
+# base url al
 r = requests.get("https://data-reality.com/domain.php")
 base = r.json()["baseurl"]
 
-# txt yaz
 with open("link.txt", "w") as f:
-    f.write(base)
+    f.write(base + "\n")
+    f.write("https://milyontv3.com/")
