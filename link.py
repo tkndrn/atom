7874a795784f5450cbe70bc2a380f7f8ca338b49
tkndrn <tekinduran77@gmail.com>
2026-03-13 14:54:40 +0300

    Delete link.py

--- link.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# base url al
-r = requests.get("https://data-reality.com/domain.php")
-base = r.json()["baseurl"]
-
-with open("link.txt", "w") as f:
-    f.write(base + "\n")
-    f.write("https://milyontv3.com/")
